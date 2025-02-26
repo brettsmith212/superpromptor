2a7b65a346fc09904e8c6fd6ba86aaa66feef084
Brett Smith <brettsmith212@gmail.com>
2025-02-25 23:41:54 -0800

    styling

--- app/superpromptor/_components/mode-selector.tsx
+++ app/superpromptor/_components/mode-selector.tsx
@@ -33,13 +33,18 @@ import XmlParser from "./xml-parser"
 
 export default function ModeSelector() {
   const [mode, setMode] = useState("template")
+  const triggerClassName = "data-[state=active]:border-b-2 data-[state=active]:border-primary data-[state=inactive]:hover:border-b-2 data-[state=inactive]:hover:border-primary/50"
 
   return (
     <div>
       <Tabs value={mode} onValueChange={setMode} className="w-full">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="template">Template Prompt Editor</TabsTrigger>
-          <TabsTrigger value="xml">XML Code Parser</TabsTrigger>
+          <TabsTrigger value="template" className={triggerClassName}>
+            Template Prompt Editor
+          </TabsTrigger>
+          <TabsTrigger value="xml" className={triggerClassName}>
+            XML Code Parser
+          </TabsTrigger>
         </TabsList>
       </Tabs>
       <div className={mode === "template" ? "" : "hidden"}>
